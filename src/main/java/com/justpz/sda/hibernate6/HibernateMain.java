a9cce0cb44d670969aa3d6761ff5e882279b2b5c
Lukasz <l.bazyluk92@gmail.com>
2019-03-23 15:36:47 +0100

    HQL ADD

--- src/main/java/com/justpz/sda/hibernate6/HibernateMain.java
+++ src/main/java/com/justpz/sda/hibernate6/HibernateMain.java
@@ -6,13 +6,14 @@ import org.hibernate.Transaction;
 
 public class HibernateMain {
     public static void main(String[] args) {
+
         try (SessionFactory sessionFactory = SessionManager.getSessionFactory();
              Session session = sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
-        Car car1 = new Car();
-        car1.setModel("Mondeo");
-        car1.setName("Ford");
-        session.persist(car1);
+            Car car1 = new Car();
+            car1.setModel("Mondeo");
+            car1.setName("Ford");
+            session.persist(car1);
 
 
             Car car = session.find(Car.class, 1L);
@@ -20,6 +21,15 @@ public class HibernateMain {
             System.out.println(car.getClass());
             car.setName("Tomcio");
             transaction.commit();
+
+            for (Car queryXX : Hibernate_HQL.queryXX()) {
+                System.out.println(queryXX);
+            }
+
+
+        } finally {
+            SessionManager.getSessionFactory().close();
         }
+
     }
 }
---
+++ src/main/java/com/justpz/sda/hibernate6/Hibernate_HQL.java
@@ -0,0 +1,27 @@
+package com.justpz.sda.hibernate6;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+
+import java.util.List;
+
+public class Hibernate_HQL {
+
+    public static List<Car> queryXX() {
+        try (SessionFactory sessionFactory = SessionManager.getSessionFactory();
+             Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            String hqlQuery = "select s from Car s where s.name = :name";
+            Query<Car> query = session.createQuery(hqlQuery, Car.class);
+            query.setParameter("name","Ferrari");
+            List<Car>list = query.list();
+            return list;
+        }
+
+
+    }
+}
+
