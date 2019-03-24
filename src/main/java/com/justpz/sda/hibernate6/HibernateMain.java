265bef62f1e896c19453fa4273b1b361891f02c9
Lukasz <l.bazyluk92@gmail.com>
2019-03-24 10:01:57 +0100

    rework

--- src/main/java/com/justpz/sda/hibernate6/HibernateMain.java
+++ src/main/java/com/justpz/sda/hibernate6/HibernateMain.java
@@ -1,28 +1,29 @@
 package com.justpz.sda.hibernate6;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
 public class HibernateMain {
     public static void main(String[] args) {
-        //        try (SessionFactory sessionFactory = SessionManager.getSessionFactory();
-//             Session session = sessionFactory.openSession()) {
+//                try (SessionFactory sessionFactory = SessionManager.getSessionFactory();
+//                     Session session = sessionFactory.openSession()) {
 //            Transaction transaction = session.beginTransaction();
 //            Car car1 = new Car();
-//            car1.setId(123L);
 //            car1.setModel("Mondeo");
 //            car1.setName("Ford");
 //            session.persist(car1);
 //
 //
-//            Car car = session.find(Car.class, 123L);
+//            Car car = session.find(Car.class,1L);
 //            System.out.println(car);
 //            System.out.println(car.getClass());
 //            car.setName("Tomcio");
 //            transaction.commit();
 //
-//            for (Car queryXX : HibernateHQL.queryXX()) {
+//            for (Car queryXX : Hibernate_HQL.queryXX()) {
 //                System.out.println(queryXX);
 //            }
 //
