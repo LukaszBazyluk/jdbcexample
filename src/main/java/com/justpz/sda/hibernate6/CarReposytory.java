82405d3430cb9c7ee1bce22c58233ab3c0a6d278
Lukasz <l.bazyluk92@gmail.com>
2019-03-24 09:31:56 +0100

    add Transaction

--- src/main/java/com/justpz/sda/hibernate6/CarReposytory.java
+++ src/main/java/com/justpz/sda/hibernate6/CarReposytory.java
@@ -1,6 +1,7 @@
 package com.justpz.sda.hibernate6;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 import java.util.List;
@@ -8,26 +9,32 @@ import java.util.List;
 public class CarReposytory {
     public List<Car> finaAllFord(){
         try (Session session = SessionManager.getSessionFactory().openSession()){
+            Transaction transaction = session.beginTransaction();
             Query<Car> query = session.createQuery("select c from Car c where c.name like :customName", Car.class);
             query.setParameter("customName", "%Mondeo%");
             List<Car> list = query.list();
+            transaction.commit();
             return list;
         }
     }
 
     public List<Car> findAll(){
         try(Session session = SessionManager.getSessionFactory().openSession()){
+            Transaction transaction = session.beginTransaction();
             Query<Car> query = session.createQuery("select c from Car c", Car.class);
             List<Car> list = query.list();
+            transaction.commit();
             return list;
         }
     }
 
     public int changeMondeoModelToFord(String name){
         try(Session session = SessionManager.getSessionFactory().openSession()){
+            Transaction transaction = session.beginTransaction();
             Query query = session.createQuery("update Car set name = :name where model = 'Ford'");
             query.setParameter("name" ,name);
             int i = query.executeUpdate();
+            transaction.commit();
             return i;
         }
     }
--- src/main/java/com/justpz/sda/hibernate6/HibernateMain.java
+++ src/main/java/com/justpz/sda/hibernate6/HibernateMain.java
@@ -1,8 +1,13 @@
-package com.justpz.sda.hibernate6;
-
+package hibernateg6;
+import com.justpz.sda.hibernate6.Car;
+import com.justpz.sda.hibernate6.CarReposytory;
+import com.justpz.sda.hibernate6.Hibernate_HQL;
+import com.justpz.sda.hibernate6.SessionManager;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+
+import java.util.List;
 
 public class HibernateMain {
     public static void main(String[] args) {
@@ -11,12 +16,13 @@ public class HibernateMain {
              Session session = sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
             Car car1 = new Car();
+            car1.setId(123L);
             car1.setModel("Mondeo");
             car1.setName("Ford");
             session.persist(car1);
 
 
-            Car car = session.find(Car.class, 1L);
+            Car car = session.find(Car.class, 123L);
             System.out.println(car);
             System.out.println(car.getClass());
             car.setName("Tomcio");
@@ -29,6 +35,15 @@ public class HibernateMain {
 
         } finally {
             SessionManager.getSessionFactory().close();
+        }
+
+        try(Session session = SessionManager.getSessionFactory().openSession()){
+            CarReposytory carRepository = new CarReposytory();
+            List<Car> all = carRepository.finaAllFord();
+
+            for(Car car : all) {
+                System.out.println(car);
+            }
         }
 
     }
