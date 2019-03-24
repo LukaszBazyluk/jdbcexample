ea0afabdb8765331f6ce679d426a6e8f80ec6778
Lukasz <l.bazyluk92@gmail.com>
2019-03-24 09:40:19 +0100

    up

--- src/main/java/com/justpz/sda/hibernate6/HibernateMain.java
+++ src/main/java/com/justpz/sda/hibernate6/HibernateMain.java
@@ -1,4 +1,5 @@
 package hibernateg6;
+
 import com.justpz.sda.hibernate6.Car;
 import com.justpz.sda.hibernate6.CarReposytory;
 import com.justpz.sda.hibernate6.Hibernate_HQL;
@@ -11,40 +12,43 @@ import java.util.List;
 
 public class HibernateMain {
     public static void main(String[] args) {
+        //        try (SessionFactory sessionFactory = SessionManager.getSessionFactory();
+//             Session session = sessionFactory.openSession()) {
+//            Transaction transaction = session.beginTransaction();
+//            Car car1 = new Car();
+//            car1.setId(123L);
+//            car1.setModel("Mondeo");
+//            car1.setName("Ford");
+//            session.persist(car1);
+//
+//
+//            Car car = session.find(Car.class, 123L);
+//            System.out.println(car);
+//            System.out.println(car.getClass());
+//            car.setName("Tomcio");
+//            transaction.commit();
+//
+//            for (Car queryXX : HibernateHQL.queryXX()) {
+//                System.out.println(queryXX);
+//            }
+//
+//
+//        } finally {
+//            SessionManager.getSessionFactory().close();
+//        }
 
-        try (SessionFactory sessionFactory = SessionManager.getSessionFactory();
-             Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
-            Car car1 = new Car();
-            car1.setId(123L);
-            car1.setModel("Mondeo");
-            car1.setName("Ford");
-            session.persist(car1);
-
-
-            Car car = session.find(Car.class, 123L);
-            System.out.println(car);
-            System.out.println(car.getClass());
-            car.setName("Tomcio");
-            transaction.commit();
-
-            for (Car queryXX : Hibernate_HQL.queryXX()) {
-                System.out.println(queryXX);
-            }
-
-
-        } finally {
-            SessionManager.getSessionFactory().close();
-        }
-
-        try(Session session = SessionManager.getSessionFactory().openSession()){
+//        --------------------------------------------------------------------------
+        try (Session session = SessionManager.getSessionFactory().openSession()) {
             CarReposytory carRepository = new CarReposytory();
             List<Car> all = carRepository.finaAllFord();
 
-            for(Car car : all) {
+            for (Car car : all) {
                 System.out.println(car);
             }
+        } finally {
+            SessionManager.getSessionFactory().close();
         }
-
+//    -------------------------------------------------------------------------------
     }
 }
+
